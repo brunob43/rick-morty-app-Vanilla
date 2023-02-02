631cb1775cf142c956e10be96a7aff89b56ebb39
bruno <bruno@kiras-MacBook-Pro.local>
2023-02-02 15:31:29 -0300

    update-js

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,18 @@
 export default class API {
+   async getCharacter(id) {
+     const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+     const data = await response.json()
+     return data
+     // .then((response) => response.json())
+     //   .then((data) => data)
+   }
+ }
+
+
+/*export default class API {
  getCharacter() {
     fetch(`https://rickandmortyapi.com/api/character/${id}`)
     .then((response)=> response.json())
     .then ((data) => console.log(data))
  }
-}
+}*/
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
-import API from "./api.js"
+import API from './api.js'
+import Character from './character.js'
 const api = new API()
+let currentCaracter = 4
+const $loadNext = document.querySelector('#load-next')
 
+$loadNext.addEventListener('click', async () => {
+  const characterData = await api.getCharacter(++currentCaracter)
+  new Character(characterData)
+})
 
+async function initApp(initCharacterId) {
+  const characterData = await api.getCharacter(initCharacterId)
+  console.log(characterData)
+  new Character(characterData)
+}
 
-console.log(api.getCharacter(1))
+initApp(currentCaracter)
